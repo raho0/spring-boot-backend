ed435771d7ea85f3ddaf9fb00495a79f6c0e079b
raho0 <rahmanisaygin@gmail.com>
2021-09-08 16:10:34 +0300

    invalidSortByRequestParamExceptionHandler added

--- src/main/java/com/backend/backend/exception/GeneralExceptionHandler.java
+++ src/main/java/com/backend/backend/exception/GeneralExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.backend.backend.exception;
 
 import com.backend.backend.dto.ExceptionDTO;
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,12 @@ public class GeneralExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ExceptionDTO missingRequestParamExceptionHandler(MissingServletRequestParameterException ex){
         return new ExceptionDTO(ex.getParameterName() + " parameter is required");
+    }
+
+    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ExceptionDTO invalidSortByRequestParamExceptionHandler(PropertyReferenceException ex){
+        return new ExceptionDTO("sortBy parameter '" + ex.getPropertyName() + "' is invalid");
     }
 
 }
