dd629b7e0ed1e15ecf74e96b3f887fa496c55755
raho0 <rahmanisaygin@gmail.com>
2021-09-08 15:04:53 +0300

    required parameter exception handler added responseStatus 400

--- src/main/java/com/backend/backend/exception/GeneralExceptionHandler.java
+++ src/main/java/com/backend/backend/exception/GeneralExceptionHandler.java
@@ -1,13 +1,15 @@
 package com.backend.backend.exception;
 
 import com.backend.backend.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
-
+    @ResponseStatus(code= HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ExceptionDTO missingRequestParamExceptionHandler(MissingServletRequestParameterException ex){
         return new ExceptionDTO(ex.getParameterName() + " parameter is required");
